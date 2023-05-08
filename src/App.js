097a5fa432aac60e056689467555e504dc2e05d8
synoce-iitgn <76461801+aman1chaudhary@users.noreply.github.com>
2023-05-08 13:15:43 +0530

    set git pages

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import Login from './components/Login'
 import Register from './components/Register'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Dashboard from './Pages/Dashboard'
 import Footer from './components/Footer';
 // import MapDashboard from './Pages/MapDashboard';
